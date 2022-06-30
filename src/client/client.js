92d142ef60211d67791ea1380011c1d2d7861195
Carmen-Git-It <carmenwhitton@gmail.com>
2022-06-30 16:04:24 -0400

    Added a tower of cubes

--- src/client/client.js
+++ src/client/client.js
@@ -18,7 +18,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.x = 0;
 camera.position.y = 7;
-camera.position.z = 10;
+camera.position.z = 25;
 
 // Set up the renderer
 const renderer = new THREE.WebGLRenderer();
@@ -39,23 +39,78 @@ world.broadphase = new CANNON.NaiveBroadphase();
 world.solver.iterations = 10;
 //world.allowSleep = true;
 
+// Add pile of cubes
+
 // Creating a cube
 const cube_geometry = new THREE.BoxGeometry(1,1,1);
-const cube_material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true});
-const cube = new THREE.Mesh(cube_geometry, cube_material);
-cube.position.x = -3;
-cube.position.y = 3;
-scene.add(cube);
+const cube_material = new THREE.MeshBasicMaterial({ color: 0xff0000});
+//Bottom left cube
+const cube1 = new THREE.Mesh(cube_geometry, cube_material);
+cube1.position.x = -1;
+cube1.position.y = 1;
+scene.add(cube1);
+//Bottom middle cube
+const cube2 = new THREE.Mesh(cube_geometry, cube_material);
+cube2.position.x = 0;
+cube2.position.y = 1;
+scene.add(cube2);
+//Bottom right cube
+const cube3 = new THREE.Mesh(cube_geometry, cube_material);
+cube3.position.x = 1;
+cube3.position.y = 1;
+scene.add(cube3);
+//middle left cube
+const cube4 = new THREE.Mesh(cube_geometry, cube_material);
+cube4.position.x = -0.5;
+cube4.position.y = 2;
+scene.add(cube4);
+//Middle right cube
+const cube5 = new THREE.Mesh(cube_geometry, cube_material);
+cube5.position.x = 0.5;
+cube5.position.y = 2;
+scene.add(cube5);
+//Top cube
+const cube6 = new THREE.Mesh(cube_geometry, cube_material);
+cube6.position.x = 0;
+cube6.position.y = 3;
+scene.add(cube6);
 
 // Create the cube's physical body in the world
 const cubeShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5));
-const cubeBody = new CANNON.Body({ mass: 1 });
-cubeBody.addShape(cubeShape);
-cubeBody.position.set(cube.position.x, cube.position.y, cube.position.z);
-world.addBody(cubeBody);
+
+// Bottom left cube
+const cubeBody1 = new CANNON.Body({ mass: 1 });
+cubeBody1.addShape(cubeShape);
+cubeBody1.position.set(cube1.position.x, cube1.position.y, cube1.position.z);
+world.addBody(cubeBody1);
+// Bottom middle cube
+const cubeBody2 = new CANNON.Body({ mass: 1 });
+cubeBody2.addShape(cubeShape);
+cubeBody2.position.set(cube2.position.x, cube2.position.y, cube2.position.z);
+world.addBody(cubeBody2);
+// Bottom right cube
+const cubeBody3 = new CANNON.Body({ mass: 1 });
+cubeBody3.addShape(cubeShape);
+cubeBody3.position.set(cube3.position.x, cube3.position.y, cube3.position.z);
+world.addBody(cubeBody3);
+// Middle left cube
+const cubeBody4 = new CANNON.Body({ mass: 1 });
+cubeBody4.addShape(cubeShape);
+cubeBody4.position.set(cube4.position.x, cube4.position.y, cube4.position.z);
+world.addBody(cubeBody4);
+// Middle right cube
+const cubeBody5 = new CANNON.Body({ mass: 1});
+cubeBody5.addShape(cubeShape);
+cubeBody5.position.set(cube5.position.x, cube5.position.y, cube5.position.z);
+world.addBody(cubeBody5);
+// Top cube
+const cubeBody6 = new CANNON.Body({ mass: 1 });
+cubeBody6.addShape(cubeShape);
+cubeBody6.position.set(cube6.position.x, cube6.position.y, cube6.position.z);
+world.addBody(cubeBody6);
 
 // Creating a plane
-const plane_geometry = new THREE.PlaneGeometry(20,20,20);
+const plane_geometry = new THREE.PlaneGeometry(500,500,500);
 const plane_material = new THREE.MeshBasicMaterial({ color:0xffffff});
 const plane = new THREE.Mesh(plane_geometry, plane_material);
 plane.rotateX(-Math.PI / 2);
@@ -83,9 +138,9 @@ world.addContactMaterial(frictionPlane);
 
 // Create sphere to acts as user control
 const sphere_geometry = new THREE.SphereGeometry(0.5, 32, 32);
-const sphere_material = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true });
+const sphere_material = new THREE.MeshBasicMaterial({ color: 0x0000ff});
 const user = new THREE.Mesh(sphere_geometry, sphere_material);
-user.position.set(0,3,0);
+user.position.set(0,3,15);
 scene.add(user);
 
 // Create the user control physics body
@@ -115,9 +170,9 @@ cameraFolder.add(camera.position, 'y', -10, 10);
 cameraFolder.add(camera.position, 'z', -10, 10);
 cameraFolder.open();
 const cubeFolder = gui.addFolder('Cube Settings');
-cubeFolder.add(cube.position, 'x', -10, 10);
-cubeFolder.add(cube.position, 'y', -10, 10);
-cubeFolder.add(cube.position, 'z', -10, 10);
+cubeFolder.add(cube1.position, 'x', -10, 10);
+cubeFolder.add(cube1.position, 'y', -10, 10);
+cubeFolder.add(cube1.position, 'z', -10, 10);
 cubeFolder.open();
 const physicsFolder = gui.addFolder('Physics Settings');
 physicsFolder.add(world.gravity, 'x', -10, 10, 0.1);
@@ -163,8 +218,19 @@ function render() {
 }
 
 function updatePositions() {
-    cube.position.copy(cubeBody.position);
-    cube.quaternion.copy(cubeBody.quaternion);
+    cube1.position.copy(cubeBody1.position);
+    cube1.quaternion.copy(cubeBody1.quaternion);
+    cube2.position.copy(cubeBody2.position);
+    cube2.quaternion.copy(cubeBody2.quaternion);
+    cube3.position.copy(cubeBody3.position);
+    cube3.quaternion.copy(cubeBody3.quaternion);
+    cube4.position.copy(cubeBody4.position);
+    cube4.quaternion.copy(cubeBody4.quaternion);
+    cube5.position.copy(cubeBody5.position);
+    cube5.quaternion.copy(cubeBody5.quaternion);
+    cube6.position.copy(cubeBody6.position);
+    cube6.quaternion.copy(cubeBody6.quaternion);
+
     plane.position.copy(planeBody.position);
     plane.quaternion.copy(planeBody.quaternion);
     user.position.copy(userBody.position);
@@ -194,7 +260,6 @@ function handleKeyDown(e) {
         // userBody.applyLocalImpulse(new CANNON.Vec3(0, 0, -1));
     }
     if (e.keyCode == 39) {
-        console.log('right');
         // Right
         userBody.applyImpulse(new CANNON.Vec3(1, 0, 0), new CANNON.Vec3(0, 0.5 / 2, 0));
         // userBody.applyLocalImpulse(new CANNON.Vec3(1, 0, 10));
@@ -204,7 +269,6 @@ function handleKeyDown(e) {
         userBody.applyImpulse(new CANNON.Vec3(0, 0, 1), new CANNON.Vec3(0, 0.5 / 2, 0));
         // userBody.applyLocalImpulse(new CANNON.Vec3(0, 0, 1));
     }
-    console.log(userBody.velocity);
 }
 
 animate();
